79c4a592af6885e212da10bcab493db0be5ecbee
Luu Viet Anh <l.rivazh@gmail.com>
2022-03-28 04:08:24 +0700

    bổ sung nội dung trang index và sửa vài chi tiết nhỏ

---
+++ access/js/frequently_use/header.js
@@ -0,0 +1,33 @@
+// Start: scroll transparent on/off
+$(window).on("scroll", function() {
+    if($(window).scrollTop() > 50) {
+        $("header").addClass("active");
+    } else {
+       $("header").removeClass("active");
+    }
+});
+// End: scroll transparent on/off
+
+window.document.onkeydown = function(e) {
+    if (!e) {
+      e = event;
+    }
+    if (e.keyCode == 27) {
+      lightbox_close();
+    }
+  }
+  
+  function lightbox_open() {
+    var lightBoxVideo = document.getElementById("VisaChipCardVideo");
+    window.scrollTo(0, 0);
+    document.getElementById('light').style.display = 'block';
+    document.getElementById('fade').style.display = 'block';
+    lightBoxVideo.play();
+  }
+  
+  function lightbox_close() {
+    var lightBoxVideo = document.getElementById("VisaChipCardVideo");
+    document.getElementById('light').style.display = 'none';
+    document.getElementById('fade').style.display = 'none';
+    lightBoxVideo.pause();
+  }
